a60b7e64b3c51bf8ec722d1f1997a096af55ab54
Joan Morell <noogic@gmail.com>
2018-09-11 12:23:14 +0200

    Plugin Container stores class names instead of instances

--- src/ApplicationRequestPluginContainer.php
+++ src/ApplicationRequestPluginContainer.php
@@ -2,17 +2,27 @@
 
 namespace Noogic\LaravelRequest;
 
+use Noogic\LaravelRequest\Exceptions\PluginNotFoundException;
+
 class ApplicationRequestPluginContainer
 {
     protected $plugins = [];
 
-    public function register(ApplicationRequestPlugin $plugin)
+    public function register($plugin, string $key)
     {
-        $this->plugins[] = $plugin;
+        $this->plugins[$key] = $plugin;
     }
 
-    public function plugins()
+    public function get($key)
     {
+        if (! isset($this->plugins[$key])) {
+            throw new PluginNotFoundException($key);
+        }
+
+        return $this->plugins[$key];
+    }
+
+    public function index() {
         return $this->plugins;
     }
 }
